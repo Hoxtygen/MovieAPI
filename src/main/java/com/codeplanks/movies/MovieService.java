a1116b2b33d95eb79ed068e916d7b1cb997fa3ca
Wasiu Idowu <hoxtygen@live.com>
2023-03-24 21:20:01 +0100

    Implement get movie by imdb id

--- src/main/java/com/codeplanks/movies/MovieService.java
+++ src/main/java/com/codeplanks/movies/MovieService.java
@@ -1,9 +1,11 @@
 package com.codeplanks.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,4 +14,10 @@ public class MovieService {
   public List<Movie> allMovies(){
    return movieRepository.findAll();
   }
+
+
+  public Optional<Movie> singleMovieByImdbId(String id) {
+    return movieRepository.findMovieByImdbId(id);
+  }
+
 }
