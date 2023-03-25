95fef5d8ec2b8c52497d61a661a859efb92f763b
Wasiu Idowu <hoxtygen@live.com>
2023-03-25 12:33:06 +0100

    Implement adding of reviews to movie

--- src/main/java/com/codeplanks/movies/ReviewRepository.java
+++ src/main/java/com/codeplanks/movies/ReviewRepository.java
@@ -1,2 +1,9 @@
-package com.codeplanks.movies;public interface ReviewRepository {
+package com.codeplanks.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
